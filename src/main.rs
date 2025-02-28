5b5dba0af874d9b5961297ecb8329bb5e6ff0c4c
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2025-02-28 12:57:11 +0100

    delegate part of the internals to cargo-tree

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,39 @@
 use cargo::{
-    core::{PackageId, Resolve, Workspace, shell::Shell},
-    ops::load_pkg_lockfile,
-    util::{command_prelude::*, important_paths},
+    core::{dependency::DepKind, shell::Shell},
+    ops::{
+        Packages, load_pkg_lockfile,
+        tree::{self, TreeOptions},
+    },
+    util::command_prelude::*,
 };
 use std::{
-    collections::{BTreeSet, HashMap},
+    collections::{BTreeSet, HashMap, HashSet},
     io::Write,
 };
 use tabwriter::TabWriter;
 
 fn main() {
-    let mut gctx = match GlobalContext::default() {
-        Ok(gctx) => gctx,
-        Err(e) => {
-            let mut shell = Shell::new();
-            cargo::exit_with_error(e.into(), &mut shell)
-        }
-    };
+     let mut gctx = GlobalContext::default().unwrap_or_else(|e| {
+        let mut eval = Shell::new();
+        cargo::exit_with_error(e.into(), &mut eval)
+    });
 
     if let Err(e) = run(&mut gctx) {
-        cargo::exit_with_error(e.into(), &mut *gctx.shell())
+        cargo::exit_with_error(e.into(), &mut gctx.shell())
     }
 }
 
+fn cli() -> Command {
+    subcommand("duplicates")
+        .about("A cargo subcommand for displaying when different versions of a same dependency are pulled in")
+        .arg_features()
+        .arg_manifest_path()
+        .arg_lockfile_path()
+}
+
 fn run(gctx: &mut GlobalContext) -> anyhow::Result<()> {
-    let root = important_paths::find_root_manifest_for_wd(gctx.cwd())?;
-    let ws = Workspace::new(&root, gctx)?;
+    let args = cli().try_get_matches()?;
+    let ws = args.workspace(gctx)?;
     let lockfile = if let Some(lockfile) = load_pkg_lockfile(&ws)? {
         lockfile
     } else {
@@ -45,67 +53,57 @@ fn run(gctx: &mut GlobalContext) -> anyhow::Result<()> {
     duplicates.sort_by_key(|d| d.0);
     if duplicates.is_empty() {
         println!("No duplicate dependencies, yay!");
-    } else {
-        let mut writer = TabWriter::new(Vec::new());
-        writeln!(&mut writer, "Package\tVersions")?;
-        writeln!(&mut writer, "-------\t--------")?;
-        for (name, deps) in &duplicates {
-            writeln!(
-                &mut writer,
-                "{}\t{}",
-                name,
-                deps.iter()
-                    .fold("".to_string(), |acc, dep| if acc.is_empty() {
-                        dep.version().to_string()
-                    } else {
-                        format!("{}\t{}", dep.version(), acc)
-                    })
-            )?;
-        }
-        writer.flush()?;
-        print!("{}", String::from_utf8(writer.into_inner()?)?);
-        for (_, deps) in &duplicates {
-            println!();
-            for dep in deps.iter().rev() {
-                println!("{} {}:", dep.name(), dep.version());
-                for c in find_dependents_of(&lockfile, *dep) {
-                    println!(
-                        "{} {} {}",
-                        c.iter()
-                            .fold("- Because of".to_string(), |acc, dep| format!(
-                                "{} {} {} =>",
-                                acc,
-                                dep.name(),
-                                dep.version()
-                            )),
-                        dep.name(),
-                        dep.version()
-                    );
-                }
-            }
-        }
+        return Ok(());
     }
-    Ok(())
-}
 
-fn find_dependents_of(lockfile: &Resolve, dep: PackageId) -> Vec<Vec<PackageId>> {
-    let mut res = Vec::new();
-    for d in lockfile.iter() {
-        if lockfile.deps(d).into_iter().filter(|d| d.0 == dep).count() != 0 {
-            let mut r = find_dependents_of(lockfile, d)
-                .iter()
-                .map(|dd| {
-                    let mut dd = dd.clone();
-                    dd.push(d);
-                    dd
+    let mut writer = TabWriter::new(Vec::new());
+    writeln!(&mut writer, "Package\tVersions")?;
+    writeln!(&mut writer, "-------\t--------")?;
+    for (name, deps) in &duplicates {
+        writeln!(
+            &mut writer,
+            "{}\t{}",
+            name,
+            deps.iter()
+                .fold("".to_string(), |acc, dep| if acc.is_empty() {
+                    dep.version().to_string()
+                } else {
+                    format!("{}\t{}", dep.version(), acc)
                 })
-                .collect::<Vec<_>>();
-            if r.is_empty() {
-                res.push(vec![d]);
-            } else {
-                res.append(&mut r);
-            }
-        }
+        )?;
     }
-    res
+    writer.flush()?;
+    println!("{}", String::from_utf8(writer.into_inner()?)?);
+
+    let duplicate_packages = duplicates
+        .iter()
+        .flat_map(|(_, deps)| {
+            deps.iter()
+                .rev()
+                .map(|dep| format!("{}@{}", dep.name(), dep.version()))
+        })
+        .collect::<Vec<String>>();
+    let mut edge_kinds = HashSet::new();
+    edge_kinds.insert(tree::EdgeKind::Dep(DepKind::Normal));
+    edge_kinds.insert(tree::EdgeKind::Dep(DepKind::Build));
+    edge_kinds.insert(tree::EdgeKind::Dep(DepKind::Development));
+
+    tree::build_and_print(
+        &ws,
+        &TreeOptions {
+            cli_features: args.cli_features()?,
+            packages: Packages::Packages(duplicate_packages.clone()),
+            target: tree::Target::All,
+            edge_kinds,
+            invert: duplicate_packages,
+            pkgs_to_prune: Default::default(),
+            prefix: tree::Prefix::Indent,
+            no_dedupe: true,
+            duplicates: false,
+            format: "{p}".to_string(),
+            graph_features: false,
+            display_depth: tree::DisplayDepth::MaxDisplayDepth(u32::MAX),
+            no_proc_macro: false,
+        },
+    )
 }
